537e0ea1a24d47c3148dac8b45063dc26ebb9d35
Ricarte13 <viniciusdiv@hotmail.com>
2023-09-19 15:11:04 -0300

    Service layer, component registration

---
+++ src/main/java/com/ricarte/projectweb/services/UserService.java
@@ -0,0 +1,27 @@
+package com.ricarte.projectweb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricarte.projectweb.entities.User;
+import com.ricarte.projectweb.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
