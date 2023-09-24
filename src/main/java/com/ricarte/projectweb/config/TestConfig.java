64ac90d64f0d5934ec5959904fb35a84a8f96af5
Ricarte13 <viniciusdiv@hotmail.com>
2023-09-24 08:48:51 -0300

    Payment, one-to-one association

--- src/main/java/com/ricarte/projectweb/config/TestConfig.java
+++ src/main/java/com/ricarte/projectweb/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ricarte.projectweb.entities.Category;
 import com.ricarte.projectweb.entities.Order;
 import com.ricarte.projectweb.entities.OrderItem;
+import com.ricarte.projectweb.entities.Payment;
 import com.ricarte.projectweb.entities.Product;
 import com.ricarte.projectweb.entities.User;
 import com.ricarte.projectweb.entities.enums.OrderStatus;
@@ -69,6 +70,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
